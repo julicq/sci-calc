6010b774c721aeed266223f2db78dd1f52723366
julianPavlov <62881010+julicq@users.noreply.github.com>
2023-07-27 12:31:53 +0000

    separate html and script

---
+++ src/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+
+    let currentValue = "";
+
+    function evaluateResult() {
+      // do not use following string in production!
+      
+      console.log('currentValue:', currentValue);
+      const convertedValue = currentValue
+        .replace("×", "*")
+        .replace("%", '*0.01')
+        .replace("÷", "/");
+      console.log('convertedValue:', convertedValue);
+      const result = eval(convertedValue);
+      currentValue = result.toString();
+      display.value = currentValue;
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+      button.addEventListener('click', function () {
+        const value = button.innerText;
+
+        if (value == "AC") {
+          currentValue = "";
+          display.value = currentValue;
+        } else if (value == "=") {
+          evaluateResult();
+        } else {
+          currentValue += value;
+          display.value = currentValue;
+        }
+      })
+    }
+  });
