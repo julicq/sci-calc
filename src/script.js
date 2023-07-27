200de75010956fefd1d2300b2c7f39d4950d1c59
julianPavlov <62881010+julicq@users.noreply.github.com>
2023-07-27 13:05:18 +0000

    implemented a few sci functions

--- src/script.js
+++ src/script.js
@@ -5,33 +5,49 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentValue = "";
 
     function evaluateResult() {
-      // do not use following string in production!
-      
-      console.log('currentValue:', currentValue);
-      const convertedValue = currentValue
-        .replace("×", "*")
-        .replace("%", '*0.01')
-        .replace("÷", "/");
-      console.log('convertedValue:', convertedValue);
-      const result = eval(convertedValue);
-      currentValue = result.toString();
-      display.value = currentValue;
+        // do not use following string in production!
+
+        console.log('currentValue:', currentValue);
+        const convertedValue = currentValue
+            .replace("×", "*")
+            .replace("%", '*0.01')
+            .replace("÷", "/")
+            .replace('sin', 'Math.sin')
+            .replace('ln', 'Math.log')
+            .replace('cos', 'Math.cos')
+            .replace('π', 'Math.PI')
+            .replace('log', 'Math.log10')
+            .replace('e', 'Math.e')
+            .replace('tan', 'Math.tan')
+            .replace('√', 'Math.sqrt');
+
+        console.log('convertedValue:', convertedValue);
+        const result = eval(convertedValue);
+        currentValue = result.toString();
+        display.value = currentValue;
     }
 
     for (let i = 0; i < buttons.length; i++) {
-      const button = buttons[i];
-      button.addEventListener('click', function () {
-        const value = button.innerText;
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const value = button.innerText;
 
-        if (value == "AC") {
-          currentValue = "";
-          display.value = currentValue;
-        } else if (value == "=") {
-          evaluateResult();
-        } else {
-          currentValue += value;
-          display.value = currentValue;
-        }
-      })
+            try {
+
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (value == "=") {
+                    evaluateResult();
+                } else {
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+            } catch (error) {
+                    console.error(error);
+                    currentValue = "ERROR";
+                    display.value = currentValue;
+                }
+            })
     }
-  });
+});
